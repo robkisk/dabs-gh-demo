5a6e81634408f7c8a460f4e9543983ccd9a3b569
robkisk <robby.kiskanyan@databricks.com>
2023-08-30 06:24:17 -0400

    my new cool feature

--- get_metrics.py
+++ get_metrics.py
@@ -7,19 +7,25 @@ import requests
 
 # test deploy from gha workflow
 
+
 @dlt.table
 def medium_metrics():
     df: DataFrame = dlt.read("medium_clean")
 
-    metricsDF = df.groupby("link").applyInPandas(get_metrics, schema="link string, claps double, readingTime double")
+    metricsDF = df.groupby("link").applyInPandas(
+        get_metrics, schema="link string, claps double, readingTime double"
+    )
 
     # Join on original data, sort by number of claps descending
-    finalDF = metricsDF.join(df, on = "link", how = "right_outer").sort(desc("claps"))
+    finalDF = metricsDF.join(df, on="link", how="right_outer").sort(
+        desc("claps")
+    )
     return finalDF
+
 
 # Get Medium page HTML and parse clap count and reading time
 def get_metrics(input_df: pd.DataFrame) -> pd.DataFrame:
-    story_url = input_df['link'][0]
+    story_url = input_df["link"][0]
     response = story = requests.get(story_url)
     story = response.content.decode("utf-8")
     try:
@@ -29,9 +35,16 @@ def get_metrics(input_df: pd.DataFrame) -> pd.DataFrame:
         rEndIndex = r.index(",")
         claps = int(c[0:clapEndIndex])
         readingTime = float(r[0:rEndIndex])
-        result = pd.DataFrame(data={'link': [story_url], 'claps': [claps], 'readingTime': [readingTime]})
+        result = pd.DataFrame(
+            data={
+                "link": [story_url],
+                "claps": [claps],
+                "readingTime": [readingTime],
+            }
+        )
     except Exception:
         print("Couldnt access URL, returning 0")
-        result = pd.DataFrame(data={'link': [story_url], 'claps': [0], 'readingTime': [0]})
+        result = pd.DataFrame(
+            data={"link": [story_url], "claps": [0], "readingTime": [0]}
+        )
     return result
-
